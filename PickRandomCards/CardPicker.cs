931ffe79afe6e284871bdb67ce88c201b9e8299b
N1K1111 <nik.glazkov2016@yandex.ru>
2022-04-19 12:23:08 +0300

    Добавьте файлы проекта.

---
+++ PickRandomCards/CardPicker.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PickRandomCards
+{
+    class CardPicker
+    {
+        static Random random = new Random();
+
+        public static string[] PickSomeCards(int numberOfCards) 
+        {
+            string[] pickedCards = new string[numberOfCards];
+            for (int i = 0; i < numberOfCards; i++)
+            {
+                pickedCards[i] = RandomValue() + "\t" + RandomSuit();
+            }
+            return pickedCards;
+        }
+
+        private static string RandomSuit()
+        {
+            int value = random.Next(1, 5);
+            if (value == 1)return "Пики";
+            if (value == 2)return "Черви";
+            if (value == 3)return "Крести";
+            return "Буби";
+        }
+
+        private static string RandomValue()
+        {
+            int value = random.Next(1,13);
+            if (value == 1) return "Туз";
+            if (value == 11) return "Валет";
+            if (value == 12) return "Дама";
+            if (value == 13) return "Король";
+            return value.ToString();
+        }
+    }
+}
